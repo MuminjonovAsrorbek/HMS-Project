d94b8d62b50ff5bc1923e3204b2fb9533e97f332
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-06-16 11:41:52 +0500

    test class

---
+++ src/main/java/uz/dev/hmsproject/test/TestClass.java
@@ -0,0 +1,16 @@
+package uz.dev.hmsproject.test;
+
+/**
+ * Created by: asrorbek
+ * DateTime: 6/16/25 11:41
+ **/
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("GitHub pull request Test");
+
+    }
+
+}
