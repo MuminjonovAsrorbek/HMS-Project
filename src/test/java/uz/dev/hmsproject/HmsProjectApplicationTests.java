11efba672b2e2fac8bbc9fbad4d7abf6406bc621
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-06-16 11:07:27 +0500

    Create Hospital Management System Project

---
+++ src/test/java/uz/dev/hmsproject/HmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dev.hmsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
