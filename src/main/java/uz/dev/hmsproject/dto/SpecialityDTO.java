cc5e200cd6d86206d4599248d980f65b691a7ad4
Eldor04 <hplaptop15dy2xxx@gmail.com>
2025-06-17 15:45:36 +0500

    SpecialityDTO created

---
+++ src/main/java/uz/dev/hmsproject/dto/SpecialityDTO.java
@@ -0,0 +1,22 @@
+package uz.dev.hmsproject.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link uz.dev.hmsproject.entity.Speciality}
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SpecialityDTO implements Serializable {
+
+    private Long id;
+
+    @NotBlank
+    private String name;
+}
