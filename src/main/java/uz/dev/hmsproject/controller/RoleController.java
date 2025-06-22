a863b137fe734661907ae84aef03255ad21bc290
Acer NitroV16 <abdulaziznurmatullayev@gmail.com>
2025-06-22 11:58:12 +0500

    role full crud!

---
+++ src/main/java/uz/dev/hmsproject/controller/RoleController.java
@@ -0,0 +1,48 @@
+package uz.dev.hmsproject.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.dev.hmsproject.dto.RoleDTO;
+import uz.dev.hmsproject.service.template.RoleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/role")
+@RequiredArgsConstructor
+public class RoleController {
+
+    private final RoleService roleService;
+
+    @GetMapping
+    public List<RoleDTO> getAll() {
+        return roleService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public RoleDTO getById(@PathVariable("id") Long id) {
+        return roleService.getById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody @Valid RoleDTO roleDTO) {
+        roleService.create(roleDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id,
+                                   @RequestBody @Valid RoleDTO roleDTO) {
+        roleService.update(id, roleDTO);
+        return ResponseEntity.ok("Role updated successfully");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        roleService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
