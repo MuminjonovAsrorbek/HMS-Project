ad555006259c78eb06b150ca7b6ad3b22b1d4128
Acer NitroV16 <abdulaziznurmatullayev@gmail.com>
2025-06-22 12:47:08 +0500

    role full crud!

--- src/main/java/uz/dev/hmsproject/service/template/RoleService.java
+++ src/main/java/uz/dev/hmsproject/service/template/RoleService.java
@@ -1,7 +1,9 @@
 package uz.dev.hmsproject.service.template;
 
 import uz.dev.hmsproject.dto.RoleDTO;
+import uz.dev.hmsproject.dto.response.PageableDTO;
 
 public interface RoleService extends BaseService<RoleDTO, Long> {
 
+    PageableDTO getAllPaginated(Integer page, Integer size);
 }
