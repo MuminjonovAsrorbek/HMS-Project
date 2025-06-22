5a124a4eec8b7df9d003c1f5833db85f999e760b
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-06-22 14:10:08 +0500

    PriceListService

--- src/main/java/uz/dev/hmsproject/service/PriceListServiceImpl.java
+++ src/main/java/uz/dev/hmsproject/service/PriceListServiceImpl.java
@@ -1,10 +1,12 @@
 package uz.dev.hmsproject.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uz.dev.hmsproject.dto.PriceListDTO;
 import uz.dev.hmsproject.entity.PriceList;
+import uz.dev.hmsproject.exception.EntityNotFoundException;
 import uz.dev.hmsproject.mapper.PriceListMapper;
 import uz.dev.hmsproject.repository.PriceListRepository;
 import uz.dev.hmsproject.service.template.PriceListService;
@@ -23,6 +25,7 @@ public class PriceListServiceImpl implements PriceListService {
 
     @Override
     public List<PriceListDTO> getAll() {
+
         return priceListRepository.findAll().stream()
                 .map(priceListMapper::toDTO)
                 .collect(Collectors.toList());
@@ -30,15 +33,16 @@ public class PriceListServiceImpl implements PriceListService {
 
     @Override
     public PriceListDTO getById(Long id) {
+
         PriceList priceList = priceListRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("PriceList not found"));
+                .orElseThrow(() -> new EntityNotFoundException("PriceList not found with id: " + id, HttpStatus.NOT_FOUND));
         return priceListMapper.toDTO(priceList);
     }
 
     @Override
     public void updatePriceBySpecialityId(Long specialityId, BigDecimal newPrice) {
         PriceList priceList = priceListRepository.findBySpecialityId(specialityId)
-                .orElseThrow(() -> new RuntimeException("PriceList not found for specialityId: " + specialityId));
+                .orElseThrow(() -> new EntityNotFoundException("PriceList not found for specialityId: " + specialityId, HttpStatus.NOT_FOUND));
         priceList.setPrice(newPrice);
         priceListRepository.save(priceList);
     }
