93446467a2481183288256ea17806bb43ba8480b
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-06-22 15:00:53 +0500

    Doctor slots

---
+++ src/main/java/uz/dev/hmsproject/repository/WorkSchedulerRepository.java
@@ -0,0 +1,13 @@
+package uz.dev.hmsproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.dev.hmsproject.entity.WorkScheduler;
+
+import java.util.Optional;
+
+@Repository
+public interface WorkSchedulerRepository extends JpaRepository<WorkScheduler, Long> {
+
+    Optional<WorkScheduler> findByUserIdAndDayOfWeek(Long id, int dayOfWeek);
+}
--- src/main/java/uz/dev/hmsproject/service/template/DoctorService.java
+++ src/main/java/uz/dev/hmsproject/service/template/DoctorService.java
@@ -2,5 +2,12 @@ package uz.dev.hmsproject.service.template;
 
 import uz.dev.hmsproject.dto.DoctorDTO;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public interface DoctorService extends BaseService<DoctorDTO, Long> {
+
+    List<LocalTime> getAvailable20MinuteSlots(Long doctorId, LocalDate date);
+
 }
