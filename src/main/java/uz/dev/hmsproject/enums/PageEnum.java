5c08d8b77e02d37a32f66781a543a0977917e560
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-07-02 11:29:21 +0500

    added pages in role

---
+++ src/main/java/uz/dev/hmsproject/enums/PageEnum.java
@@ -0,0 +1,21 @@
+package uz.dev.hmsproject.enums;
+
+/**
+ * Created by: asrorbek
+ * DateTime: 7/2/25 11:09
+ **/
+
+public enum PageEnum {
+
+    DOCTOR,
+    PATIENT,
+    ROOM,
+    ROLE,
+    LOG,
+    SPECIALITY,
+    WORK_SCHEDULER,
+    PRICE_LIST,
+    APPOINTMENT,
+    USER,
+    STATISTICS
+}
