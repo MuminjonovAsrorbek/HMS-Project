69ec8466d042e8bd14a8e124ace226a9068107ad
Asrorbek <muminjonovasrorbek2004@gmail.com>
2025-07-02 20:03:16 +0500

    Add swagger information in AppointmentController

--- src/main/java/uz/dev/hmsproject/controller/AppointmentController.java
+++ src/main/java/uz/dev/hmsproject/controller/AppointmentController.java
@@ -1,6 +1,7 @@
 package uz.dev.hmsproject.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -75,6 +76,23 @@ public class AppointmentController {
 
     }
 
+    @Operation(
+            summary = "Get Appointment by ID",
+            description = "Get Appointment by ID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AppointmentDTO.class),
+                            examples = @ExampleObject(
+                                    value = "{ \"id\": 1, \"patient\": \"John Doe\", \"doctor\": \"Dr. Smith\", \"dateTime\": \"2024-06-10T10:10\", \"status\": \"SCHEDULED\" , \"room\": \"12\" , \"price\": \"256000\"}"
+                            ))
+            }),
+            @ApiResponse(responseCode = "404", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Entity not found with ID:."
+                    ))
+            })
+    })
     @PreAuthorize("hasAuthority('APPOINTMENTS_READ')")
     @GetMapping("/{id}")
     public AppointmentDTO getAppointmentById(@PathVariable Long id) {
@@ -83,9 +101,30 @@ public class AppointmentController {
 
     }
 
+
+    @Operation(
+            summary = "Getting the Appointment lists to Filter",
+            description = "Getting the Appointment lists to Filter"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AppointmentDTO.class),
+                            examples = @ExampleObject(
+                                    value = "[ { \"id\": 1, \"patient\": \"John Doe\", \"doctor\": \"Dr. Smith\", \"dateTime\": \"2024-06-10T10:10\", \"status\": \"SCHEDULED\" , \"room\": \"12\" , \"price\": \"256000\"} ]"
+                            ))
+            }),
+            @ApiResponse(responseCode = "404", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Entity not found with ID:."
+                    ))
+            })
+    })
     @PreAuthorize("hasAuthority('APPOINTMENTS_READ')")
     @GetMapping("/filter")
-    public List<AppointmentDTO> filterAppointments(AppointmentFilterRequest appointmentFilterRequest) {
+    public List<AppointmentDTO> filterAppointments(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Get Appointments by filter information",
+            content = @Content(schema = @Schema(implementation = AppointmentFilterRequest.class), mediaType = "application/json")
+    ) AppointmentFilterRequest appointmentFilterRequest) {
 
         return appointmentService.filterAppointments(appointmentFilterRequest);
 
@@ -93,7 +132,34 @@ public class AppointmentController {
 
     @PreAuthorize("hasAuthority('APPOINTMENTS_UPDATE')")
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateAppointment(@PathVariable Long id,
+    @Operation(
+            summary = "Update Appointment",
+            description = "Update Appointment"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Appointment updated successfully"
+                    ))
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "The doctor isn't active now"
+                    ))
+            }),
+            @ApiResponse(responseCode = "404", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Entity not found with ID:"
+                    ))
+            })
+    })
+    public ResponseEntity<?> updateAppointment(@Parameter(description = "Appointment ID", example = "1") @PathVariable Long id,
+                                               @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                                       description = "Update appointment information", required = true,
+                                                       content = @Content(
+                                                               schema = @Schema(implementation = CreateAppointmentDTO.class),
+                                                               mediaType = "application/json")
+                                               )
                                                @RequestBody @Valid CreateAppointmentDTO appointmentDTO) {
 
         appointmentService.updateAppointment(id, appointmentDTO);
@@ -104,7 +170,28 @@ public class AppointmentController {
 
     @PreAuthorize("hasAuthority('APPOINTMENTS_DELETE')")
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteAppointment(@PathVariable Long id) {
+    @Operation(
+            summary = "Delete Appointment",
+            description = "Appointment will not be deleted simply Canceled"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Appointment deleted successfully"
+                    ))
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "The doctor isn't active now"
+                    ))
+            }),
+            @ApiResponse(responseCode = "404", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Entity not found with ID:"
+                    ))
+            })
+    })
+    public ResponseEntity<?> deleteAppointment(@Parameter(example = "1") @PathVariable Long id) {
 
         appointmentService.deleteAppointment(id);
 
@@ -114,8 +201,29 @@ public class AppointmentController {
 
     @PreAuthorize("hasAuthority('APPOINTMENTS_CHANGE_STATUS')")
     @PatchMapping("/{id}/status")
-    public ResponseEntity<?> changeAppointmentStatus(@PathVariable Long id,
-                                                     @RequestParam String status) {
+    @Operation(
+            summary = "Change Appointment Status",
+            description = "Change Appointment Status"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Appointment status updated successfully"
+                    ))
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Status cannot be null or empty"
+                    ))
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Appointment cannot be canceled less than 1 hour before it starts."
+                    ))
+            }),
+    })
+    public ResponseEntity<?> changeAppointmentStatus(@Parameter(description = "Appointment ID", example = "1") @PathVariable Long id,
+                                                     @Parameter(description = "Appointment status", example = "CANCELED") @RequestParam String status) {
 
         appointmentService.changeAppointmentStatus(id, status);
 
@@ -125,7 +233,39 @@ public class AppointmentController {
 
     @PreAuthorize("hasAuthority('APPOINTMENTS_READ')")
     @GetMapping("/today-appointments")
-    public PageableDTO getTodayAppointments(@RequestParam(value = "page", defaultValue = "0") Integer page) {
+    @Operation(
+            summary = "Get Today Appointments",
+            description = "Here the values are passed in Peagable view"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AppointmentDTO.class),
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                              "size": 1,
+                                              "totalElements": 1,
+                                              "totalPages": 1,
+                                              "hasNext": false,
+                                              "hasPrevious": false,
+                                              "object": [
+                                                {
+                                                  "id": 1,
+                                                  "patient": "John Doe",
+                                                  "doctor": "Dr. Smith",
+                                                  "dateTime": "2024-06-10T10:10",
+                                                  "status": "SCHEDULED",
+                                                  "room": "12",
+                                                  "price": "256000"
+                                                }
+                                              ]
+                                            }
+                                            """
+                            ))
+            })
+    })
+    public PageableDTO getTodayAppointments(@Parameter(description = "Page number", example = "1")
+                                            @RequestParam(value = "page", defaultValue = "0") Integer page) {
 
         return appointmentService.getTodayAppointments(page);
 
@@ -133,7 +273,12 @@ public class AppointmentController {
 
     @PreAuthorize("hasAuthority('APPOINTMENTS_READ')")
     @GetMapping("/daily-appointments/export")
-    public void exportDailyAppointmentsToExcel(@RequestParam(required = false) LocalDate date,
+    @Operation(
+            summary = "Get Daily Appointments to Excel",
+            description = "Daily Appointments list can be downloaded in excel format"
+    )
+    public void exportDailyAppointmentsToExcel(@Parameter(description = "Enter the required date.", example = "2025-07-05")
+                                               @RequestParam(required = false) LocalDate date,
                                                HttpServletResponse response) throws IOException {
 
         if (Objects.isNull(date)) date = LocalDate.now();
@@ -155,7 +300,43 @@ public class AppointmentController {
 
     @PreAuthorize("hasAuthority('APPOINTMENTS_UPDATE')")
     @PutMapping("/reschedule")
-    public ResponseEntity<?> reschedule(@RequestBody @Valid AppointmentRescheduleRequest dto) {
+    @Operation(
+            summary = "Update Appointment",
+            description = "This update allows you to assign a patient to a new doctor or change the date."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Appointment rescheduled successfully"
+                    ))
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Can change Appointment only in Scheduled mode"
+                    ))
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "Appointments can only be scheduled for today or future dates."
+                    ))
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "The doctor isn't active now"
+                    ))
+            }),
+            @ApiResponse(responseCode = "409", content = {
+                    @Content(mediaType = "application/json", examples = @ExampleObject(
+                            value = "This time slot is already booked"
+                    ))
+            }),
+    })
+    public ResponseEntity<?> reschedule(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Update appointment information", required = true,
+            content = @Content(
+                    schema = @Schema(implementation = AppointmentRescheduleRequest.class),
+                    mediaType = "application/json"))
+                                        @RequestBody @Valid AppointmentRescheduleRequest dto) {
 
         appointmentService.reschedule(dto);
 
