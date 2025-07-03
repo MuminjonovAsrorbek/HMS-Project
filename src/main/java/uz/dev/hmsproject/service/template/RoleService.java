441cec06db5bc175b8a76c4fe0e37f6e9510ffe7
Acer NitroV16 <abdulaziznurmatullayev@gmail.com>
2025-07-03 15:13:09 +0500

    new swagger changes and role

--- src/main/java/uz/dev/hmsproject/service/template/RoleService.java
+++ src/main/java/uz/dev/hmsproject/service/template/RoleService.java
@@ -2,8 +2,15 @@ package uz.dev.hmsproject.service.template;
 
 import uz.dev.hmsproject.dto.RoleDTO;
 import uz.dev.hmsproject.dto.response.PageableDTO;
+import uz.dev.hmsproject.enums.Permissions;
+
+import java.util.List;
 
 public interface RoleService extends BaseService<RoleDTO, Long> {
 
     PageableDTO getAllPaginated(Integer page, Integer size);
+
+    List<Permissions> getAllPermissions();
+
+    List<Permissions> getPermissionsById(Long id);
 }
